88b89d7b94391ee68d2df70ebb6fefbd2d6fbb36
Stephen SOLOMON <stephensololmon48@gmail.com>
2020-04-13 15:24:07 +0100

    updated estimator.js file

--- src/estimator.js
+++ src/estimator.js
@@ -16,42 +16,29 @@ const normalizedDays = (periodType, periodCount) => {
   return days;
 };
 
-const ci = (reportedCases, severe = false) => {
-  return !severe ? reportedCases * 10 : reportedCases * 50;
-};
+const ci = (reportedCases, severe = false) => !severe ? reportedCases * 10 : reportedCases * 50;
 
-const ibrt = (currentlyInfected, days) => {
-  return currentlyInfected * (2 ** Math.trunc(days / 3));
-};
+const ibrt = (currentlyInfected, days) => currentlyInfected * (2 ** Math.trunc(days / 3));
 
-const scbrt = (ibrt) => {
-  return Math.trunc(ibrt * 0.15);
-};
+const scbrt = (infected) => Math.trunc(infected * 0.15);
 
 const hbbrt = (thb, { severeCasesByRequestedTime }) => {
-  return Math.trunc((thb * 0.35) - severeCasesByRequestedTime);
+  const calc = Math.trunc((thb * 0.35) - severeCasesByRequestedTime);
+  return calc;
 };
 
-const cfibrt = ({ infectionsByRequestedTime }) => {
-  return Math.trunc(infectionsByRequestedTime * 0.05);
-};
+const cfibrt = ({ infectionsByRequestedTime }) => Math.trunc(infectionsByRequestedTime * 0.05);
 
-const cfvbrt = ({ infectionsByRequestedTime }) => {
-  return Math.trunc(infectionsByRequestedTime * 0.02);
-};
+const cfvbrt = ({ infectionsByRequestedTime }) => Math.trunc(infectionsByRequestedTime * 0.02);
 
-const dif = ({ infectionsByRequestedTime }, { avgDailyIncomeInUSD, avgDailyIncomePopulation }, day) => {
-  return infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * day;
-};
-
-const estimator = (x) => {
-  
-};
+const dif = ({ infectionsByRequestedTime }, 
+  { avgDailyIncomeInUSD, avgDailyIncomePopulation }, 
+  day) => infectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * day;
 
 const covid19ImpactEstimator = (data) => {
   const impact = {};
   const severeImpact = {};
-  let days = normalizedDays(data.periodType, data.timeToElapse);
+  const days = normalizedDays(data.periodType, data.timeToElapse);
   
   impact.currentlyInfected = ci(data.reportedCases);
   severeImpact.currentlyInfected = ci(data.reportedCases, true);
@@ -72,7 +59,7 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.casesForVentilatorsByRequestedTime = cfvbrt(severeImpact);
 
   impact.dollarInFlight = dif(impact, data.region, days);
-  severeImpact.dollarInFlight = dif(severe, data.region, days);
+  severeImpact.dollarInFlight = dif(severeImpact, data.region, days);
 };  
 
 export default covid19ImpactEstimator;
